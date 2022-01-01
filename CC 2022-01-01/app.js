4ddd93b3b8baf0feafb477fc2d6a6211a1326257
Leia Stephenson <thehautebunny@gmail.com>
2022-01-01 13:26:51 -0500

    Coding challenge and solution for 1-1-22 coding challenge.

---
+++ CC 2022-01-01/app.js
@@ -0,0 +1,5 @@
+//Convert a Boolean to a String
+//Implement a function which convert the given boolean value into its string representation.
+function booleanToString(b){
+    return b.toString()
+}
