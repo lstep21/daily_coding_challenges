02ab62ce1ff7ed61c7a78a6ce1cb1c50e61d6060
Leia Stephenson <thehautebunny@gmail.com>
2022-01-04 13:34:33 -0500

    Coding challenge and solution for 1-4-22 coding challenge.

---
+++ CC 2022-01-04/app.js
@@ -0,0 +1,5 @@
+//Reversed Words
+//Complete the solution so that it reverses all of the words within the string passed in.
+function reverse(str) {
+    return str.split(' ').reverse().join(' ')
+}
