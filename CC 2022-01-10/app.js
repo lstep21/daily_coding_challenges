09ef96d3bf36fa789587e59014b3aaaed16bc725
Leia Stephenson <thehautebunny@gmail.com>
2022-01-10 14:06:31 -0500

    Coding challenge and solution for 1-10-22 coding challenge.

---
+++ CC 2022-01-10/app.js
@@ -0,0 +1,9 @@
+function lovefunc(num1, num2){
+    if (num1 % 2 === 0 && num2  % 2 === 1) {
+        return true 
+    } else if (num1 % 2 === 1 && num2  % 2 === 0) {
+        return true 
+    } else {
+        return false 
+    }
+}
