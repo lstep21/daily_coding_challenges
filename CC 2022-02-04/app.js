6fb0aacefb8e6d4ad0582857f10b7c8cdf03cfd6
Leia Stephenson <thehautebunny@gmail.com>
2022-02-04 14:12:15 -0500

    Coding challenge and solution for 2-4-22 coding challenge.

---
+++ CC 2022-02-04/app.js
@@ -0,0 +1,9 @@
+//Find Maximum and Minimum Values of a List
+//Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+function min(arr) {
+    return Math.min(...arr)
+}
+
+function max(arr) {
+    return Math.max(...arr)
+}
