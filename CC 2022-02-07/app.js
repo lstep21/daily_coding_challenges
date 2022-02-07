5c946bad8bbf5954491de9d21f824360d52080fa
Leia Stephenson <thehautebunny@gmail.com>
2022-02-07 14:16:33 -0500

    Coding challenge and solution got 2-7-22 coding challenge.

---
+++ CC 2022-02-07/app.js
@@ -0,0 +1,6 @@
+//Sum Mixed Array
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//Return your answer as a number.
+function sumMix(x){
+    return x.reduce((sum, currentValue) => sum + Number(currentValue), 0)
+}
