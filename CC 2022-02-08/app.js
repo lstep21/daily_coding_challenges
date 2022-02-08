530cdedd0a2febe5babec5037fc2c4e18cb89bd9
Leia Stephenson <thehautebunny@gmail.com>
2022-02-08 14:18:03 -0500

    Coding challenge and solution for 2-8-22 coding challenge.

---
+++ CC 2022-02-08/app.js
@@ -0,0 +1,5 @@
+//Remove exclamation marks
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+function removeExclamationMarks(str) {
+    return str.split('!').join('')
+}
