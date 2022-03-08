18dcd79182aaa906b0044e2982a920de145824dc
Leia Stephenson <thehautebunny@gmail.com>
2022-03-08 14:52:01 -0500

    Coding challenge and solution for 3-8-22 coding challenge.

---
+++ CC 2022-03-08/app.js
@@ -0,0 +1,6 @@
+//Sum The Strings
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+//"4",  "5" --> "9"
+function sumStrings(a,b) {
+    return String(Number(a) + Number(b))
+}
