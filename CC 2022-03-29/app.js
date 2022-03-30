f19a453b161eef82f40e3030e0ed502dba9bfb70
Leia Stephenson <thehautebunny@gmail.com>
2022-03-29 20:04:01 -0400

    Coding challenge and solution for 3-29-22 coding challenge.

---
+++ CC 2022-03-29/app.js
@@ -0,0 +1,5 @@
+//Enumerable Magic #25 - Take the First N Elements
+//Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+function firstNElements(arr,n) {
+    return arr.slice(0,n)
+}
