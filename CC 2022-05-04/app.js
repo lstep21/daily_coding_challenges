7165baf70bc133fc1a996dec69e8798c95f553c9
Leia Stephenson <thehautebunny@gmail.com>
2022-05-04 15:27:49 -0400

    Coding challenge and solution for 5-4-22 coding challenge.

---
+++ CC 2022-05-04/app.js
@@ -0,0 +1,8 @@
+//Grasshopper - Messi goals function
+// Complete the function to return his total number of goals in all three leagues.
+// Note: the input will always be valid.
+// For example:
+// 5, 10, 2  -->  17
+function goals (a,b,c) {
+    return a + b + c
+ }
