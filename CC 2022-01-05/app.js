baa7f2fb0d23db550dae9ad1938805a5e72f509d
Leia Stephenson <thehautebunny@gmail.com>
2022-05-14 14:58:15 -0400

    Updated coding challenge and solution for 1-5-22 coding challenge

--- CC 2022-01-05/app.js
+++ CC 2022-01-05/app.js
@@ -1,15 +1,5 @@
 //Fake Binary
 //Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 function fakeBinary(str) {
-    let arr = str.split('')
-    let binary = arr.map(
-        num => {
-            if (num < 5) {
-                return 0
-            } else {
-                return 1
-            }
-        }
-    )
-    return binary
+    return str.split('').map(num => num < 5 ? 0 : 1).join('')
 }
