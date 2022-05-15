7baf5ea6fee0b973dc016fb726a548dbdc402ccc
Leia Stephenson <thehautebunny@gmail.com>
2022-05-15 14:32:39 -0400

    Coding challenge and solution for 5-15-22 coding challenge.

---
+++ CC 2022-05-15/app.js
@@ -0,0 +1,8 @@
+//For Twins: 1. Types
+//Write function typeValidation that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+// Examples:
+// typeValidation(42, "number");   // => true
+// typeValidation("42", "number"); // => false
+function typeValidation (variable, type) {
+    return typeof variable === type
+}
