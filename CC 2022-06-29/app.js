9c955151ad90af29ad84f14080e0d66fcff17644
Leia Stephenson <thehautebunny@gmail.com>
2022-06-29 21:31:34 -0400

    Coding challenge and solution for 06-29-22 coding challenge.

---
+++ CC 2022-06-29/app.js
@@ -0,0 +1,6 @@
+//get ascii value of character
+// Get ASCII value of a character.
+// For the ASCII table you can refer to http://www.asciitable.com/
+function getASCII(c){
+    return c.charCodeAt(0)
+}
